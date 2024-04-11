2979f68c4736074bc8ba610182e7f0b87a77e12c
LeonSilva15 <l30silva.o@gmail.com>
2024-04-10 20:45:12 -0600

    demonstrate selecting a row using a character string

--- 01.retrieving-restricting-sorting/02.restricting_and_sorting.sql
+++ 01.retrieving-restricting-sorting/02.restricting_and_sorting.sql
@@ -2,3 +2,8 @@
 SELECT job_id
 FROM employees
 WHERE department_id = 90;
+
+-- Select a row using a character string
+SELECT last_name, job_id, department_id
+FROM employees
+WHERE last_name = 'Whalen';
