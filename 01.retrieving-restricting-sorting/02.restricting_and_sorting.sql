ee54fbf95c790a1f13320c1bfe8a19848b910e16
LeonSilva15 <l30silva.o@gmail.com>
2024-04-10 20:45:34 -0600

    demonstrate selecting a row using a date value

--- 01.retrieving-restricting-sorting/02.restricting_and_sorting.sql
+++ 01.retrieving-restricting-sorting/02.restricting_and_sorting.sql
@@ -7,3 +7,8 @@ WHERE department_id = 90;
 SELECT last_name, job_id, department_id
 FROM employees
 WHERE last_name = 'Whalen';
+
+-- Select a row using a date value
+SELECT last_name, job_id, department_id, hire_date
+FROM employees
+WHERE hire_date = '21-MAY-17';
